e8d4fed4fe5e959da92e8b8cbc47bedc302beef4
kdraganik <karol.draganik@gmail.com>
2021-05-10 18:32:11 +0200

    zajecia10/05/2021: restrukturyzacja kodu

--- Notes/notes/routes.py
+++ Notes/notes/routes.py
@@ -1,16 +1,8 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request, redirect
 from flask_login import login_user
-from forms import LoginForm, RegistrationForm
-from config import Config
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+from notes import app, db
+from notes.forms import LoginForm, RegistrationForm
+from notes.models import User
 
 @app.route('/')
 def hello_world():
@@ -19,7 +11,7 @@ def hello_world():
 @app.route('/login', methods=["POST", "GET"])
 def login():
     form = LoginForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         #dodać sprawdzenie
         if user and user.password == form.password.data:
@@ -30,12 +22,10 @@ def login():
 @app.route('/register', methods=["POST", "GET"])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         #dodać haszowanie
         user = User(username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template("register.html", title="register", form=form)
-
-app.run(debug=True)
